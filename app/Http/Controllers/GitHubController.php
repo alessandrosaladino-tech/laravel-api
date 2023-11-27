6caaf2b07eb8ce4208231c5130078d1cb658fc7d
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-27 16:36:16 +0100

    Fix

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -12,48 +12,51 @@ class GitHubController extends Controller
 {
     public function fetchRepositories()
     {
-        $author = env('GITHUB_USERNAME', 'alessandrosaladino-tech');
+        $author = env('GITHUB_USERNAME', 'AlessandroSaladino');
 
-        $response = Http::withoutVerifying()->withHeader('Authorization', 'Bearer ' . env('GITHUB_API_TOKEN'))->get("https://api.github.com/users/{$author}/repos?sort=created&direction=asc&per_page=100");
+
+
+        $response = Http::withoutVerifying()->get("https://api.github.com/users/alessandrosaladino-tech/repos?sort=created&direction=asc&per_page=100");
+
 
         $repositories = $response->json();
 
         foreach ($repositories as $repository) {
+
             $project = Project::updateOrCreate(
                 [
                     'title' => $repository['name']
                 ],
                 [
+                    'title' => $repository['name'],
                     'slug' => Project::generateSlug($repository['name']),
                     'description' => $repository['description'],
-                    'cover_image' => 'placeholders/ilya-pavlov-OqtafYT5kTw-unsplash.jpg',
+                    'cover_image' => 'placeholders/L3PymiCpURxhE12JTZwHsKpF46u8cGKbiwihdXIA.png',
                     'github_link' => $repository['html_url']
                 ]
             );
 
-            $lang_response = Http::withoutVerifying()->withHeader('Authorization', 'Bearer ' . env('GITHUB_API_TOKEN'))->get("https://api.github.com/repos/{$author}/{$repository['name']}/languages");
+            //sostituire la richiesta quando le chiamate a github si sbloccano
+            $lang_response = Http::withoutVerifying()->get("https://api.github.com/repos/{$author}/{$repository['name']}/languages");
+            //$lang_response = Http::withoutVerifying()->get("https://5247f9c2-b00d-4d81-b23b-aaf483dd9534.mock.pstmn.io/https://api.github.com/repos/MarcoZellini/laravel-boolean/languages");
 
-            if ($lang_response->successful()) {
+            $project_technologies = $lang_response->json();
 
-                $languagesData = $lang_response->json();
+            foreach ($project_technologies as $language) {
+                $technology = Technology::updateOrCreate(
+                    ['name' => $language],
+                    [
+                        'slug' => Technology::generateSlug($language)
+                    ],
+                );
 
-                $totalSize = array_sum($languagesData);
-
-                foreach ($languagesData as $language => $size) {
-                    $percentage = ($size / $totalSize) * 100;
-
-                    $technology = Technology::firstOrCreate(
-                        ['name' => $language],
-                        [
-                            'slug' => Technology::generateSlug($language),
-                        ]
-                    );
-                }
-
-                
-                $project->type_id = rand(1, 5);
-                $project->save();
+                $technology_ids[] = Technology::where('slug', $technology->slug)->first()->id;
             }
+
+            $project->technologies()->sync($technology_ids);
+
+            //TODO: Generate a project type based on languages
+            $project->type_id = rand(1, 5);
         }
         return to_route('admin.projects.index')->with('message', 'Fetch Data From GitHub Successfully');
     }
